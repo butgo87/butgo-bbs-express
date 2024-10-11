0eddeb1b60a3fc49d3e13d0a089869313a522c02
sjbyun <b317201@nate.com>
2024-10-11 17:00:21 +0900

    db 스크립트 삭제

--- db_scsript/dml.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT INTO T_SEQUENCE
-(SEQ_NAME, NEXT_VAL, SEQ_DATE, PREFIX, PADDING, CD_PREFIX_TYPE, CD_CYCLE_OPTION, CD_REC_STAT, FRST_REG_USER_ID, FRST_REG_DTTM, LAST_UDT_USER_ID, LAST_UDT_DTTM)
-VALUES('USER_SEQ', 1, datetime('now','localtime'), NULL, 10, 'O', 'D', '1', '0', datetime('now','localtime'), '0', datetime('now','localtime'));
-
-
